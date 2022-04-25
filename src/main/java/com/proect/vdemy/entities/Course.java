2712045d8292100babcacae5a4dceb2d0c9cc45b
Surajit Karuri <surajitkaruri@gmail.com>
2022-04-25 09:38:09 +0530

    added lombok dependency

--- src/main/java/com/proect/vdemy/entities/Course.java
+++ src/main/java/com/proect/vdemy/entities/Course.java
@@ -1,8 +1,13 @@
 package com.proect.vdemy.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 public class Course {
 
     @Id
@@ -12,55 +17,4 @@ public class Course {
     @Column(name = "description")
     private String desc;
     private Integer price;
-
-    public Course() {
-    }
-
-    public Course(String title, String desc, Integer price) {
-        this.title = title;
-        this.desc = desc;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Course{" +
-                "Id=" + id +
-                ", title='" + title + '\'' +
-                ", desc='" + desc + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }
